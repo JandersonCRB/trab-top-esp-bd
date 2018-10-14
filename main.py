df8a8614530002c1bdd3a29aec3e8da27a9a2682
JandersonCRB <jandersonangelo@hotmail.com>
2018-10-14 18:53:12 -0300

    bugfixes

--- main.py
+++ main.py
@@ -12,41 +12,38 @@ def products_dict():
         description = product.find('description').text
         SellPriceMax = product.find('SellPriceMax').text
         SellPriceMin = product.find('SellPriceMin').text
+        razaoSocialName = product.find('razaoSocialName').text
 
-        product_dict[id] = { 'id'           : id,
-                             'description'  : description,
-                             'SellPriceMax' : SellPriceMax,
-                             'SellPriceMin' : SellPriceMin}
+        product_dict[id] = { 'id'             : id,
+                             'description'    : description,
+                             'SellPriceMax'   : SellPriceMax,
+                             'SellPriceMin'   : SellPriceMin,
+                             'razaoSocialName': razaoSocialName}
 
     return product_dict
-
-
 
 
 def stores_dict():
     tree = ET.parse('./DB/products.xml')
     root = tree.getroot()
-
     stores = {}
     for product in root.findall('product'):
-        node = product.find('razaoSocialName')
-        if node is not None:
-            razaoSocialName = node.text
-            fantasiaName = product.find('fantasiaName').text
-            neighborhood = product.find('neighborhood').text
-            cepNum = product.find('cepNum').text
-            cityName = product.find('cityName').text
-            SellPriceMax = product.find('SellPriceMax').text
-            SellPriceMin = product.find('SellPriceMin').text
-            longitudeNum = product.find('longitudeNum').text
-            stores[razaoSocialName] = {'razaoSocialName': razaoSocialName,
-                                         'fantasiaName': fantasiaName,
-                                         'neighborhood': neighborhood,
-                                         'cepNum': cepNum,
-                                         'cityName': cityName,
-                                         'SellPriceMax': SellPriceMax,
-                                         'SellPriceMin': SellPriceMin,
-                                         'latitudeNum': longitudeNum}
+        razaoSocialName = product.find('razaoSocialName').text
+        fantasiaName = product.find('fantasiaName').text
+        neighborhood = product.find('neighborhood').text
+        cepNum = product.find('cepNum').text
+        cityName = product.find('cityName').text
+        SellPriceMax = product.find('SellPriceMax').text
+        SellPriceMin = product.find('SellPriceMin').text
+        longitudeNum = product.find('longitudeNum').text
+        stores[razaoSocialName] = {'razaoSocialName': razaoSocialName,
+                                   'fantasiaName': fantasiaName,
+                                   'neighborhood': neighborhood,
+                                   'cepNum': cepNum,
+                                   'cityName': cityName,
+                                   'SellPriceMax': SellPriceMax,
+                                   'SellPriceMin': SellPriceMin,
+                                   'latitudeNum': longitudeNum}
     return stores
 
 

ea36913f9d8c88d54cdf375addd76f6db281a881
Marcus Vinicius <marcuspestana@ic.ufal.br>
2018-10-18 22:39:21 -0300

    Interface working 1.0

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import xml.etree.ElementTree as ET
+import sys
+right_price_logo = "###################################################################################\n############                        RIGHT$PRICE                        ############\n###################################################################################"
+
+
+
+
 
 
 def products_dict():
@@ -20,8 +26,27 @@ def products_dict():
 
     return product_dict
 
-
-
+def search_id_list(id_list):
+    result_list = []
+    tree = ET.parse("./DB/products.xml")
+    root = tree.getroot()
+    products = root.findall('product')
+    for product_id in id_list:
+        product = products.find('[id='+product_id+']')
+        description = product['description']
+        SellPriceMax = product['description']
+        SellPriceMin = product['description']
+        addressName = product['description']
+        fantasiaName = product['description']
+        neighborhood = product['description']
+        result_list.append({'id'              : products.id,
+                            'description'     : description,
+                            'SellPriceMax'    : SellPriceMax,
+                            'SellPriceMin'    : SellPriceMin,
+                            'addressName'     : addressName,
+                            'razaoSocialName' : fantasiaName,
+                            'neighborhood'    : neighborhood
+                            })
 
 def stores_dict():
     tree = ET.parse('./DB/products.xml')
@@ -49,10 +74,63 @@ def stores_dict():
                                          'latitudeNum': longitudeNum}
     return stores
 
+def shop_list_menu():
+    pass
+
+def search_product(name):
+    product_list = ["1 - Rola de " + name, "2 - Pica de " + name, "3 - Buceta de " + name,
+                    "4 - Sua Mae de " + name, "5 - Minha vo de " + name, "6 - Ta no face " + name]
+    return product_list
+
+def add_product_to_shop_list(product_id):
+    print product_id + (" adicionado com sucesso!")
+
+
+def search_product_menu():
+    sys.stdout.flush()
+    print right_price_logo
+    print("\n\n")
+
+    print("Digite o nome do produto:\n")
+    product_name = raw_input()
+    products_list = search_product(product_name)
+    for product in products_list:
+        print product
+    print("\nDigite o codigo do produto que deseja adicionar a sua lista de compras:")
+    product_id = raw_input()
+    add_product_to_shop_list(product_id)
+
+def intro_menu():
+    sys.stdout.flush()
+    print right_price_logo
+    print("\n\n")
+    print("Qual seu nome?")
+
+    nome = raw_input()
+
+    return nome
 
 def main():
-    print("Hello World")
+    logged_off = True
+    exit_program = False
+    while exit_program is False:
+        if logged_off is True:
+            nome = intro_menu()
+        logged_off = False
+        print("Bem-vindo, ") + nome + '!\n'
+        print("O que deseja fazer?\n\n"
+              "1) Procurar produto\n"
+              "2) Lista de Compras\n"
+              "3) Sair")
+        resposta = raw_input()
 
+        if resposta == '1':
+            search_product_menu()
+        if resposta == '2':
+            shop_list_menu()
+        if resposta == '3':
+            print "\nAte mais, " + nome + '!'
+            exit_program = True
 
 if __name__ == "__main__":
     main()
